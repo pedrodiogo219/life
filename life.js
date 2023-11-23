14e314da4fe2f9303022ebdbdeb1610f8398b2f9
Pedro Diogo <pedrodiogo219@gmail.com>
2023-11-24 00:34:02 +0100

    looking pretty

--- life.js
+++ life.js
@@ -1,16 +1,27 @@
 const ROWS = 10;
 const COLS = 10;
+const ALIVE = true;
+const DEAD = false;
+let state;
+let table;
 
 function createInitialState() {
     const state = new Array();
     for(let i = 0; i < ROWS; i++){
         newRow = new Array();
         for (let j = 0; j < COLS; j++){
-            newRow.push(0);
+            newRow.push(DEAD);
         }
         state.push(newRow);
     }
     return state;
+}
+
+function swapCell(i, j){
+    state[i][j] = !state[i][j];
+    state[i][j] ? 
+        table[i][j].className = "cell cell-alive" :
+        table[i][j].className = "cell cell-dead"
 }
 
 function createTable(){
@@ -22,6 +33,7 @@ function createTable(){
         for (let j = 0; j < COLS; j++){
             newCell = newRowElement.insertCell();
             newCell.className = "cell cell-dead"
+            newCell.onclick = () => swapCell(i, j)
             newRow.push(newCell)
         }
         tableElement.appendChild(newRowElement);
@@ -40,22 +52,18 @@ function drawState(state, table){
     }
 }
 
-function go() {
-    let state = createInitialState();
-    let table = createTable();
-    
-    state = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-             [0, 0, 0, 1, 1, 1, 0, 0, 0, 0],
-             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-             [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
-             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
+state = createInitialState();
+table = createTable();
 
-    drawState(state, table)
-}
+state = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+         [0, 0, 0, 1, 1, 1, 0, 0, 0, 0],
+         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+         [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
 
-go()
+drawState(state, table)
